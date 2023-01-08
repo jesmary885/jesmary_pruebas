8543d098a76743966d1ac9608ee839b91afd15dd
Jesmary Carneiro <82709587+jesmary885@users.noreply.github.com>
2023-01-08 06:38:16 -0500

    Update Bloc.php

--- app/Http/Livewire/Footer/Bloc.php
+++ app/Http/Livewire/Footer/Bloc.php
@@ -31,7 +31,7 @@ class Bloc extends Component
             }
     
             else{
-                $this->emit('error','Tu bloc se excede de la cantidad de caracteres permitido');
+                $this->emit('error','Tu bloc se excede de ');
             }
 
         return view('livewire.footer.bloc');
