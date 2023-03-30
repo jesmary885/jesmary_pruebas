829ab3c17fd3848deeddc62409a1aa2dae00b1a5
Jesmary Carneiro <jesmary885@gmail.com>
2023-03-30 18:45:49 -0400

    30-03-23

--- resources/views/livewire/admin/links-generados.blade.php
+++ resources/views/livewire/admin/links-generados.blade.php
@@ -9,6 +9,7 @@
                 <table class="table text-sm table-bordered table-responsive-lg table-responsive-md table-responsive-sm">
                     <thead class="text-xs uppercase bg-gray-700 text-gray-400">
                         <tr>
+                            <th class="text-center py-3">Fecha</th>
                             <th class="text-center py-3">K</th>
                             <th class="text-center p">LINK</th>
                             <th class="text-center p">USUARIO</th>
@@ -18,6 +19,10 @@
                         <tbody>
                             @foreach ($jumpers as $jumper)
                                 <tr class="bg-gray-800 border-gray-700 hover:bg-gray-600">
+
+                                    <td class="py-3 text-center font-medium whitespace-nowrap text-white">
+                                    {{\Carbon\Carbon::parse($jumper->created_at)->format('d-m-Y h:i')}}
+                                    </td>
                                   
                                     <td class="text-center py-3 px-2">{{$jumper->k_detected}}</td>
                                     <td class="text-center py-3 px-2">
