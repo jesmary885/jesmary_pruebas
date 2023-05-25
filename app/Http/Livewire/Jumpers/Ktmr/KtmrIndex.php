63f58db942fa985559cd49f5ace686c8fa9bd344
Jesmary Carneiro <jesmary885@gmail.com>
2023-05-24 22:55:39 -0400

    24-05-23

--- app/Http/Livewire/Jumpers/Ktmr/KtmrIndex.php
+++ app/Http/Livewire/Jumpers/Ktmr/KtmrIndex.php
@@ -43,7 +43,7 @@ class KtmrIndex extends Component
   
                 $client = new Client([
                     //'base_uri' => 'http://127.0.0.1:8000',
-                    'base_uri' => 'http://209.94.57.88/',
+                    'base_uri' => 'http://146.190.74.228/',
                 ]);
     
                 $resultado = $client->request('GET', '/ktmr/1/'.$this->psid_buscar);
